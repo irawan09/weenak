d9561a200cc1b2d5c74f2278479a5c0781904b14
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2021-12-09 13:20:13 +0100

    add JSON data preprocessing

--- app/src/main/java/irawan/electroshock/weenak/MainActivity.kt
+++ app/src/main/java/irawan/electroshock/weenak/MainActivity.kt
@@ -89,10 +89,9 @@ class MainActivity : ComponentActivity() {
                 return when {
                     actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
                     actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                    //for other device how are able to connect with Ethernet
                     actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                    //for check internet over Bluetooth
                     actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> true
                     else -> false
                 }
             } else {
