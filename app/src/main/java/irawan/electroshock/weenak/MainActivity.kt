062b959341884daa0b6151e4f275f8c79b79eccf
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2021-12-17 10:23:40 +0100

    Image Authorization Problem

--- app/src/main/java/irawan/electroshock/weenak/MainActivity.kt
+++ app/src/main/java/irawan/electroshock/weenak/MainActivity.kt
@@ -5,8 +5,11 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Card
@@ -21,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import coil.compose.rememberImagePainter
 import irawan.electroshock.weenak.model.RecipeModel
 import irawan.electroshock.weenak.ui.theme.WeenakTheme
 import irawan.electroshock.weenak.view_model.DataViewModel
@@ -95,16 +99,33 @@ fun DataCard(recipeModel: List<RecipeModel>?) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight()
-                    .padding(8.dp)
+                    .padding(4.dp)
                     .wrapContentSize(Alignment.TopStart)){
-                        Card(elevation = 4.dp, modifier = Modifier.padding(all = 1.dp)){
+                        Card(elevation = 4.dp, modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(all = 1.dp)
+                            .clickable { }){
 
                             var foodName = recipeModel[index].foodName
                             var foodImage = recipeModel[index].foodImage
+                            Log.d("data Image", foodImage)
                             var foodDescription = recipeModel[index].foodDescription
-                            Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(all = 1.dp)){
-//                                Image()
-                                Column(modifier = Modifier.padding(8.dp)) {
+                            Column( modifier = Modifier.padding(all = 1.dp)){
+                                Box(modifier = Modifier
+                                    .fillMaxWidth()
+                                    .fillMaxHeight(), contentAlignment = Alignment.Center){
+                                    val painter = rememberImagePainter(data = foodImage, builder = {})
+                                    Image(
+                                        painter = painter,
+                                        contentDescription = null,
+                                        modifier = Modifier
+                                            .fillMaxHeight()
+                                            .fillMaxWidth())
+                                }
+
+
+
+                                Column(modifier = Modifier.padding(2.dp)) {
                                     Text(text = foodName, fontSize = 12.sp, fontWeight = FontWeight.Bold)
                                     Text(text = foodDescription, fontSize = 10.sp)
                                 }
