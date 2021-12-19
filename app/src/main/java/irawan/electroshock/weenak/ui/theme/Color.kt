8b3cfded9cbcb8c2955be1cda0859711a66bdc3c
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2021-12-19 18:44:35 +0100

    Compose UI Update

--- app/src/main/java/irawan/electroshock/weenak/ui/theme/Color.kt
+++ app/src/main/java/irawan/electroshock/weenak/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package irawan.electroshock.weenak.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
+val Purple200 = Color(0xFF03A9F4)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
