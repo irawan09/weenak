c0439c1526bd1fd3a9a6ade30b310506982be1b0
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2021-12-22 13:52:55 +0100

    Working on Fragment

---
+++ app/src/main/java/irawan/electroshock/weenak/view/fragment/RecipeDetailsFragment.kt
@@ -0,0 +1,27 @@
+package irawan.electroshock.weenak.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class RecipeDetailsFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = ComposeView(requireContext())
+        view.apply {
+            setContent {
+                Text("Hello")
+            }
+        }
+
+        return view
+    }
+}
