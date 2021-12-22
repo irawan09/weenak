9d9262a17400d446e701b0284049f8957d4c353f
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2021-12-22 11:53:07 +0100

    Working on details Compose UI

--- app/src/main/java/irawan/electroshock/weenak/view/DetailsRecipe.kt
+++ app/src/main/java/irawan/electroshock/weenak/view/DetailsRecipe.kt
@@ -1,14 +1,17 @@
 package irawan.electroshock.weenak.view
 
 import android.util.Log
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import irawan.electroshock.weenak.model.RecipeModel
 
 fun detailsRecipe(details : RecipeModel) {
-    Log.d("Data clicked ", details.foodName.toString())
+    Log.d("Data clicked ", details.foodName)
+//    DetailsDisplay(input = details.foodName)
 }
 
 @Composable
-fun DetailsDisplay(){
+fun DetailsDisplay(input : String){
+    Text(text = input)
 
 }
